5e2c8fec9f08a9bc745233aa859deaa761229c6a
Peter Westerström <peter.westerstrom@eonreality.com>
2020-12-22 20:30:22 +0100

    Make tuple_math on tuples only and not any template arg

--- include/westerstrom/tuple_math.h
+++ include/westerstrom/tuple_math.h
@@ -7,37 +7,98 @@
 
 #include <cstddef>
 #include <tuple>
+#include <utility>
 
 namespace westerstrom
 {
+	template <size_t i = 0, typename F, typename... Ts>
+	constexpr void tuple_map(std::tuple<Ts...>& a, F f)
+	{
+		constexpr auto N = std::tuple_size_v<std::tuple<Ts...>>;
+		if constexpr(i < N)
+		{
+			f(std::get<i>(a));
+			tuple_map<i + 1>(a, f);
+		}
+	}
+
 	namespace detail
 	{
-		template <size_t i, typename T> constexpr void add_impl(T& a, const T& b)
+		template <size_t i, typename... Ts>
+		constexpr void add_impl(std::tuple<Ts...>& a, const std::tuple<Ts...>& b)
 		{
-			constexpr auto N = std::tuple_size_v<T>;
+			constexpr auto N = std::tuple_size_v<std::tuple<Ts...>>;
 			if constexpr(i < N)
 			{
 				std::get<i>(a) += std::get<i>(b);
 				add_impl<i + 1>(a, b);
 			}
 		}
+
 	} // namespace detail
 
-	template <typename T> constexpr auto add(T a, const T& b)
+	template <typename... Ts> constexpr auto add(std::tuple<Ts...> a, const std::tuple<Ts...>& b)
 	{
-		detail::add_impl<0, T>(a, b);
+		detail::add_impl<0, Ts...>(a, b);
 		return a;
 	}
 
-	template <typename T> constexpr auto operator+(T a, const T& b)
+	template <typename... Ts>
+	constexpr auto operator+(std::tuple<Ts...> a, const std::tuple<Ts...>& b)
 	{
 		return add(a, b);
 	}
 
-	template <typename T> constexpr auto& operator+=(T& a, const T& b)
+	template <typename... Ts>
+	constexpr auto& operator+=(std::tuple<Ts...>& a, const std::tuple<Ts...>& b)
 	{
-		detail::add_impl<0, T>(a, b);
+		detail::add_impl<0, Ts...>(a, b);
 		return a;
 	}
+
+	template <typename S, typename... Ts>
+	constexpr auto& operator*=(std::tuple<Ts...>& a, const S& scalar)
+	{
+		tuple_map(a, [scalar](auto& t) { t *= scalar; });
+		return a;
+	}
+
+	template <typename S, typename... Ts>
+	constexpr auto operator*(const S& scalar, std::tuple<Ts...> a)
+	{
+		a *= scalar;
+		return a;
+	}
+
+	template <typename S, typename... Ts>
+	constexpr auto operator*(std::tuple<Ts...> a, const S& scalar)
+	{
+		a *= scalar;
+		return a;
+	}
+
+	template <typename... Ts> constexpr auto& operator-=(std::tuple<Ts...>& a, std::tuple<Ts...> b)
+	{
+		b *= -1;
+		a += b;
+		return a;
+	}
+
+	template <typename... Ts>
+	constexpr auto operator-(std::tuple<Ts...> a, const std::tuple<Ts...>& b)
+	{
+		a -= b;
+		return a;
+	}
+
+	// product
+	template <typename... Ts> auto tuple_manhattan(std::tuple<Ts...> a, std::tuple<Ts...> b)
+	{
+		a -= b;
+		std::tuple_element_t<0, std::tuple<Ts...>> sum{};
+		tuple_map(a, [&sum](auto x) { sum += abs(x); });
+		return sum;
+	}
+
 } // namespace westerstrom
 #endif // TUPLE_MATH_H
--- test/tuple_math.cpp
+++ test/tuple_math.cpp
@@ -2,15 +2,14 @@
 // Created by Peter Westerstrom on 2020-12-18.
 //
 
+#include <catch2/catch.hpp>
 #include <westerstrom/tuple_math.h>
 
-#include <catch2/catch.hpp>
+using std::make_tuple;
+using namespace westerstrom;
 
 TEST_CASE("tuple add", "[add]")
 {
-	using std::make_tuple;
-	using namespace westerstrom;
-
 	REQUIRE(add(make_tuple(1, 10), make_tuple(2, 20)) == make_tuple(3, 30));
 
 	REQUIRE((make_tuple(1, 10) + make_tuple(2, 20)) == make_tuple(3, 30));
@@ -18,4 +17,46 @@ TEST_CASE("tuple add", "[add]")
 	std::tuple<int, int, float> v{10, -5, 3.14f};
 	v += make_tuple(20, 1, 4.1f);
 	REQUIRE(v == make_tuple(30, -4, 7.24f));
+}
+
+TEST_CASE("tuple minus", "[minus]")
+{
+	REQUIRE((make_tuple(1, 10) - make_tuple(2, 20)) == make_tuple(-1, -10));
+}
+
+TEST_CASE("tuple scalar mul", "[tuple_math]")
+{
+	auto a = make_tuple(2, 3);
+	a *= 3;
+	REQUIRE(a == make_tuple(6, 9));
+}
+
+TEST_CASE("tuple tuple mul assignment", "[tuple_math]")
+{
+	auto a = make_tuple(2, 3);
+	auto b = make_tuple(11, 13);
+	a += b;
+	REQUIRE(a == make_tuple(13, 16));
+}
+
+TEST_CASE("tuple scalar tuple mul", "[tuple_math]")
+{
+	auto a = make_tuple(2, 3);
+	auto c = 5 * a;
+	REQUIRE(c == make_tuple(10, 15));
+}
+
+TEST_CASE("tuple tuple add", "[tuple_math]")
+{
+	auto a = make_tuple(2, 3);
+	auto b = make_tuple(11, 13);
+	auto c = a + b;
+	REQUIRE(c == make_tuple(13, 16));
+}
+
+TEST_CASE("tuple manhattan distance")
+{
+	auto a = make_tuple(2, 3);
+	auto b = make_tuple(11, 13);
+	REQUIRE(tuple_manhattan(a, b) == 19);
 }
